f824466f14aa8be336074dfd7ac7f693a3e3c631
kweiwen.tseng <kweiwen.tseng@gmail.com>
2023-06-29 10:00:00 +0800

    initial message

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,242 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+DecoderAudioProcessor::DecoderAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    addParameter(level1 = new juce::AudioParameterFloat("0x01", "lv1", 0.00f, 1.00f, 0.50f));
+    addParameter(level2 = new juce::AudioParameterFloat("0x02", "lv2", 0.00f, 1.00f, 0.50f));
+    addParameter(fc     = new juce::AudioParameterFloat("0x03", "fc",  0.01f, 20.0f, 5.00f));
+
+    auto b0 = 3.844633506779265e-06;
+    auto b1 = 7.68926701355853e-06;
+    auto b2 = 3.844633506779265e-06;
+    auto a0 = 1.0;
+    auto a1 = -1.9944464105419266;
+    auto a2 = 0.9944617890759537;
+
+    juce::IIRCoefficients coeffs(b0, b1, b2, a0, a1, a2);
+    instance.smoother.setCoefficients(coeffs);
+    instance.C = 0.0f;
+    instance.S = 0.0f;
+    instance.factor = 0.5f;
+}
+
+DecoderAudioProcessor::~DecoderAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String DecoderAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool DecoderAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DecoderAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DecoderAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double DecoderAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int DecoderAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int DecoderAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void DecoderAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String DecoderAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void DecoderAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void DecoderAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+}
+
+void DecoderAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool DecoderAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void DecoderAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    auto blockSize = buffer.getNumSamples();
+
+    auto* inputL = buffer.getReadPointer(0);
+    auto* inputR = buffer.getReadPointer(1);
+    auto* outputL = buffer.getWritePointer(0);
+    auto* outputR = buffer.getWritePointer(1);
+    update_coeffs(instance, fc->get(), getSampleRate());
+    for (int i = 0; i < blockSize; i++)
+    {
+        auto L = inputL[i];
+        auto R = inputR[i];
+
+        process(instance, L, R);
+
+        outputL[i] = instance.C * level1->get() - instance.S * level2->get();
+        outputR[i] = instance.C * level1->get() + instance.S * level2->get();
+    }
+}
+
+//==============================================================================
+bool DecoderAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* DecoderAudioProcessor::createEditor()
+{
+    return new DecoderAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void DecoderAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void DecoderAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new DecoderAudioProcessor();
+}
+
+void process(decoder &self, float L, float R)
+{
+    auto VCA_L = L * sin(self.factor * M_PI / 2);
+    auto VCA_R = R * sin((1 - self.factor) * M_PI / 2);
+
+    self.C = VCA_L + VCA_R;
+    self.S = VCA_L - VCA_R;
+
+    double FWR_L = fabs(VCA_L);
+    double FWR_R = fabs(VCA_R);
+
+    double E = FWR_L + FWR_R;
+
+    bool flag = (FWR_L * FWR_R > 0) && !isnan(FWR_R / E);
+
+    self.factor = self.smoother.processSingleSampleRaw(fmin(flag, (FWR_R / E)) + (1 - flag) * self.factor);
+}
+
+void update_coeffs(decoder& self, float fc, float sr)
+{
+    double q = sqrt(2) / 2;
+    float gain = 1.0;
+    double omega = 2 * M_PI * fc / sr;
+    double sineOmega = sin(omega);
+    double cosineOmega = cos(omega);
+
+    double alpha = sineOmega / (2 * q);
+    auto a0_temp = 1 + alpha;
+    auto a1 = (-2 * cosineOmega) / a0_temp;
+    auto a2 = (1 - alpha) / a0_temp;
+
+    auto b0 = (1 - cosineOmega) * gain / 2 / a0_temp;
+    auto b1 = (1 - cosineOmega) * gain / a0_temp;
+    auto b2 = (1 - cosineOmega) * gain / 2 / a0_temp;
+
+    auto a0 = 1.0;
+
+    juce::IIRCoefficients new_coeffs(b0, b1, b2, a0, a1, a2);
+    self.smoother.setCoefficients(new_coeffs);
+}
